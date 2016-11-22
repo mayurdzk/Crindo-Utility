90924b712b7f714516b518d78d0be6e11e5deaf1
Mayur <jhonmathews@gmail.com>
2016-11-22 15:09:02 +0530

    Initial Commit

---
+++ CrindoUtilities/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CrindoUtilities
+//
+//  Created by Mayur on 22/11/16.
+//  Copyright © 2016 Mayur. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CrindoUtilities/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CrindoUtilities
+//
+//  Created by Mayur on 22/11/16.
+//  Copyright © 2016 Mayur. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
