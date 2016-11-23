e2582f9482dacae51aba3c5b9f13594a182d6112
Mayur <jhonmathews@gmail.com>
2016-11-23 13:20:49 +0530

    - Separated the padding time into a variable.

--- CrindoUtilities/ViewController.swift
+++ CrindoUtilities/ViewController.swift
@@ -20,20 +20,23 @@ class ViewController: NSViewController {
         didSet {
         // Update the view, if already loaded.
         }
-        
     }
     @IBAction func copyToClipBoardButtonPressed(_ sender: NSButton) {
+        let paddingTime = 15
+        
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
         dateFormatter.timeZone = TimeZone(identifier: "IST")
         
         let calendar = Calendar.current
-        let date = calendar.date(byAdding: .second, value: 15, to: Date())
+        let date = calendar.date(byAdding: .second, value: paddingTime, to: Date())
         var dateString = dateFormatter.string(from: date!)
         
+        //Drop everything in the string after '+'
         if let dotRange = dateString.range(of: "+") {
             dateString.removeSubrange(dotRange.lowerBound..<dateString.endIndex)
         }
+        
         print(dateString)
         NSPasteboard.general().clearContents()
         NSPasteboard.general().setString(dateString, forType: NSPasteboardTypeString)
