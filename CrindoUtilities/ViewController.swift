065a6989cd34252570ef5d23f784abf1eb37bd3f
Mayur <jhonmathews@gmail.com>
2016-11-23 12:04:01 +0530

    Added a button that copies date-time for 'now' with 15 seconds added in the yyyy-MM-dd'T'HH:mm:ssZ format.

--- CrindoUtilities/ViewController.swift
+++ CrindoUtilities/ViewController.swift
@@ -20,8 +20,23 @@ class ViewController: NSViewController {
         didSet {
         // Update the view, if already loaded.
         }
+        
     }
-
-
+    @IBAction func copyToClipBoardButtonPressed(_ sender: NSButton) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        dateFormatter.timeZone = TimeZone(identifier: "IST")
+        
+        let calendar = Calendar.current
+        let date = calendar.date(byAdding: .second, value: 15, to: Date())
+        var dateString = dateFormatter.string(from: date!)
+        
+        if let dotRange = dateString.range(of: "+") {
+            dateString.removeSubrange(dotRange.lowerBound..<dateString.endIndex)
+        }
+        print(dateString)
+        NSPasteboard.general().clearContents()
+        NSPasteboard.general().setString(dateString, forType: NSPasteboardTypeString)
+    }
 }
 
